e688197539101d6d8266be46694055f60c9fccad
Sylvester de Koning <sylvester.de.koning90@gmail.com>
2016-05-16 12:08:43 +0200

    added simple means model

--- scripts/means_model.R
+++ scripts/means_model.R
@@ -1,3 +1,31 @@
 load("./data/data.RData")
 
-# Building a submission file 
+# Dependencies.
+
+library(reshape2)
+
+
+# Building a submission file using the means of the coordinates.
+
+pred <- matrix(data = colMeans(training_set, na.rm = TRUE),
+               nrow = nrow(test_set),
+               ncol = ncol(training_set),
+               byrow = TRUE)
+
+colnames(pred) <- names(training_set)
+
+predictions <- data.frame(ImageId = 1:nrow(test_set), pred)
+
+
+# Changing to submission format.
+
+submission <- melt(predictions, 
+                   id.vars = "ImageId",
+                   variable.name = "FeatureName",
+                   value.name = "Location")
+
+
+# Saving it to csv.
+
+names <- colnames(lookup_table)
+lookup_table$Location <- NULL
